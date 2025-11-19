b86f913d4a8d7ffb7b59770531286299c3f5033f
Giacomo Orsucci <orsucci.giacomo@gmail.com>
2025-11-19 14:48:06 +0100

    Boids random initialization (position and speed)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <memory>
 #include <SFML/Window.hpp>
+#include <bits/stdc++.h>
 
 struct Boid {
     float x, y;
@@ -9,13 +10,14 @@ struct Boid {
 };
 
 float squared_distance(Boid a, Boid b);
+float random_float(float min, float max);
 
 int main() {
     constexpr int N = 4;
 
     std::unique_ptr<Boid[]> boids(new Boid[N]);
 
-    // FIX ME: all necessary?
+    //Constants definition
     constexpr float TURN_FACTOR = 0.2f;
     constexpr float VISUAL_RANGE = 40;
     constexpr float PROTECTED_RANGE = 8;
@@ -24,9 +26,6 @@ int main() {
     constexpr float MATCHING_FACTOR= 0.05;
     constexpr float MAX_SPEED= 6;
     constexpr float MIN_SPEED= 3;
-    constexpr float MAX_BIAS= 0.01;
-    constexpr float BIAS_INC=0.00004;
-    constexpr float BIAS_VAL=0.001;
     constexpr float TOP_MARGIN = 600;
     constexpr float BOT_MARGIN = 800;
     constexpr float LEFT_MARGIN = 0;
@@ -34,29 +33,52 @@ int main() {
 
     float speed = 0;
 
+    //boids initialization
+    for (int i=0; i < N; i++) {
+        boids[i].x = random_float(LEFT_MARGIN, RIGHT_MARGIN);
+        boids[i].y = random_float(BOT_MARGIN, TOP_MARGIN);
 
+        boids[i].vx = random_float(MIN_SPEED, MAX_SPEED);
+        boids[i].vy = random_float(MIN_SPEED, MAX_SPEED);
+    }
 
+    //Graphical window creation
     sf::Window window(sf::VideoMode({800, 600}), "Boids simulation");
     window.setFramerateLimit(60); // call it once after creating the window
 
     while (window.isOpen())
     {
+        while (const std::optional event = window.pollEvent())
+        {
+
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        //To score every boid
         for (int i=0; i<N; i++) {
+
+            //Variables definition
             float dx =0, dy = 0, sqd=0, close_dx=0, close_dy=0, x_avg=0,
             y_avg=0, xv_avg=0, yv_avg=0, n_neighbours=0;
 
+            //To compare every boid with every one else (circular logic)
             for (int j=(i+1); j != i; j = (j+1) % N) {
+
+
                 dx = boids[i].x - boids[j].x;
                 dy = boids[i].y - boids[j].y;
 
                 if (std::fabs(dx)<VISUAL_RANGE && std::fabs(dy)<VISUAL_RANGE) {
 
                     sqd = squared_distance(boids[i], boids[j]);
+
                     if (sqd < PROTECTED_RANGE*PROTECTED_RANGE) {
+                        //Distance from near boids
                         close_dx += dx;
                         close_dy += dy;
-
-                    }else {
+                    //if not in protected range, check the visual one
+                    }else if (sqd < VISUAL_RANGE*VISUAL_RANGE) {
                         x_avg += dx;
                         y_avg += dy;
                         xv_avg += boids[j].vx;
@@ -66,6 +88,7 @@ int main() {
 
                 }
             }
+            //If there are boids in the visual range, make the boid goes to their center
             if (n_neighbours > 0) {
                 x_avg /= n_neighbours;
                 y_avg /= n_neighbours;
@@ -78,6 +101,7 @@ int main() {
             boids[i].vx += boids[i].vx + close_dx*AVOID_FACTOR;
             boids[i].vy += boids[i].vy + close_dy*AVOID_FACTOR;
 
+            //Verification of edges condition
             if (boids[i].y > TOP_MARGIN)
                 boids[i].vy += TURN_FACTOR;
             if (boids[i].y < BOT_MARGIN)
@@ -86,6 +110,7 @@ int main() {
                 boids[i].vx += TURN_FACTOR;
             if (boids[i].x < RIGHT_MARGIN)
                 boids[i].vx -= TURN_FACTOR;
+
             speed = sqrt(pow(boids[i].vx, 2) + pow(boids[i].vy, 2));
 
             if (speed < MIN_SPEED) {
@@ -105,6 +130,16 @@ int main() {
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
+
+
+float random_float(float min, float max) {
+
+    static std::mt19937 gen(std::random_device{}());
+    std::uniform_real_distribution<float> dist(min, max);
+    return dist(gen);
+}
+
 float squared_distance(Boid a, Boid b){
     return static_cast<float>(pow((a.x - b.x), 2) + pow(a.y - b.y, 2));
 }
+
